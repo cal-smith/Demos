2f247cba1ff22b5f6f8edea0385fd1a89b33108b
Callum Smith <callum1234moo@gmail.com>
2014-05-21 19:43:08 -0400

    Added a couple things i've been playing with
    see: bauhaus.html

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Demos
 Various HTML, CSS, and Javascript demos.
 
 More or less for my own entertainment.
+
+Not guaranteed to work across browsers.
---
+++ canvas.js
@@ -0,0 +1,6 @@
+var c = document.getElementById("test");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#B435D0";
+ctx.fillRect(50,100,150,75);
+ctx.fillRect(20,160,150,75);
+ctx.fillRect(100,300,200,5);
